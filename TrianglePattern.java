7433ce7810c74a173ea65a9135056acb33660408
Sushma Kharat <kharatsushma000@gmail.com>
2025-12-07 20:41:17 +0530

    Implement lower inverted triangle in pattern
    Added missing code for printing the lower inverted triangle.

--- TrianglePattern.java
+++ TrianglePattern.java
@@ -3,14 +3,14 @@
 
 public class TrianglePattern {
     public static void main(String[] args) {
-        // Upper triangle
+        
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++)
                 System.out.print(j);
             System.out.println();
         }
 
-        // Lower inverted triangle
+       
         for (int i = 4; i >= 1; i--) {
             for (int j = 1; j <= i; j++)
                 System.out.print(j);
